114fabe5c28712046133527609fa884259126e9c
Jesus Berdugo <berdugopertuz@gmail.com>
2023-05-17 17:48:35 -0500

    Creacion del archivo y rotuer Perfil

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import PrincipalView from '../views/PrincipalView/PrincipalView.vue'
+import PerfilViewVue from '@/views/PerfilView/PerfilView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +9,10 @@ const router = createRouter({
       path: '/',
       name: 'principal',
       component: PrincipalView
+    },
+    {
+      path: '/perfil',
+      component: PerfilViewVue
     }
   ]
 })
---
+++ src/views/PerfilView/PerfilView.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>Hola Mundo!</div>
+</template>
+
+<script lang="ts" setup>
+</script>
+
+<style scoped>
+</style>
