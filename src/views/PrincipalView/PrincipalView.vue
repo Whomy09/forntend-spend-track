6377286ec57166ac448ea8968ba19326976f553e
Jesus Berdugo <berdugopertuz@gmail.com>
2023-05-17 17:31:59 -0500

    Estructura de carpetas de componentes

--- src/views/PrincipalView/PrincipalView.vue
+++ src/views/PrincipalView/PrincipalView.vue
@@ -3,9 +3,9 @@
     <div class="row">
       <Header title="Gastos"/>
     </div>
-    <div class="bg-light-emphasis h-75 mt-5 p-2">
-      <button class="btn btn-primary">Agregar</button>
-      <div class=" bg-primary">
+    <div class="bg-light-emphasis h-75 mt-5">
+      <button class="btn btn-light btn-lg border border-1 shadow-sm">Agregar</button>
+      <div class="d-flex justify-content-center align-items-center bg-light mt-4 h-75 rounded-2">
         Hola
       </div>
     </div>
