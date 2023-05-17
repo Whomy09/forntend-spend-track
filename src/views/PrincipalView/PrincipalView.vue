a3aae331b664a01a73961be4c03baacea7a8dc34
Jesus Berdugo <berdugopertuz@gmail.com>
2023-05-17 17:35:44 -0500

    Prueba de ramas

--- src/views/PrincipalView/PrincipalView.vue
+++ src/views/PrincipalView/PrincipalView.vue
@@ -5,7 +5,7 @@
     </div>
     <div class="bg-light-emphasis h-75 mt-5">
       <button class="btn btn-light btn-lg border border-1 shadow-sm">Agregar</button>
-      <div class="d-flex justify-content-center align-items-center bg-light mt-4 h-75 rounded-2">
+      <div class="d-flex justify-content-center align-items-center bg-primary mt-4 h-75 rounded-2">
         Hola
       </div>
     </div>
